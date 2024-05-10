64b6820b0b77f97b752931b0e527ad76c413d4dc
eunji <103646256+ejk0731@users.noreply.github.com>
2024-05-10 18:56:28 +0900

    add: #3.2~3.6 - Typescript

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# react-master-class
-nomadcoders
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import React from "react";
-
-const plus = (a: number, b: number) => a + b;
-plus(1, 1);
-plus(1, "a");
+import { useState } from "react";
+// import styled from "styled-components";
 
 function App() {
+  // const Container = styled.div`
+  //   background-color: ${(props) => props.theme.bgColor};
+  // `;
+  // const H1 = styled.h1`
+  //   color: ${(props) => props.theme.textColor};
+  // `;
+
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+    console.log(event.currentTarget.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
-    <>
-      <></>
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        {/* input 작성후 enter시 submit됨 console로 확인가능 */}
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder="Username"
+        ></input>
+      </form>
+      {/* <Container>
+        <H1>Protected</H1>
+      </Container> */}
+    </div>
   );
 }
 
