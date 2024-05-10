2f1be356efda812b71980a727a45053a33a8f79a
eunji <103646256+ejk0731@users.noreply.github.com>
2024-05-10 17:54:46 +0900

    add: #3.0 ~ 3.1 - Typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const plus = (a: number, b: number) => a + b;
+plus(1, 1);
+plus(1, "a");
+
+function App() {
+  return (
+    <>
+      <></>
+    </>
+  );
+}
+
+export default App;
