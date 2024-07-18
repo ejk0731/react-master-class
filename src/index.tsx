2d3417f33f9b43d912ef1a7a0a446fb6abb26ca3
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-07-18 14:15:31 +0900

    chore: delete unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
+import {ThemeProvider} from "styled-components";
 import {lightTheme} from "./theme";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+  <ThemeProvider theme={lightTheme}>
     <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  </ThemeProvider>
 );
