f154a9cf17b5978466fdda39a2aa9467794b803c
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-07-25 14:45:32 +0900

    chore: setup for To Do List

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const queryClient = new QueryClient();
 root.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </RecoilRoot>
 );
