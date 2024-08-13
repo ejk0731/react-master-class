d86cea241c2af55b9963d23cede4a9cd86808de7
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-08-13 18:56:08 +0900

    feature: #6.13 - 14 : Categories, Immutability part One

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,10 +1,40 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}
-  <button>To Do</button>
-  <button>Doing</button>
-  <button>Done</button>
-  </li>;
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log(oldToDo, newToDo);
+      return oldToDos;
+    });
+  };
+  return (
+    <li>
+      {text}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 export default ToDo;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -3,9 +3,37 @@ import CreateToDo from "./CreateToDo";
 import { toDoState } from "../atoms";
 import ToDo from "./ToDo";
 
+// [
+//   {
+//     text: " sd",
+//     id: 1723542190734,
+//     category: "TO_DO",
+//   },
+//   {
+//     text: "a a",
+//     id: 1723542187782,
+//     category: "TO_DO",
+//   },
+//   {
+//     text: "a ",
+//     id: 1723542185973,
+//     category: "TO_DO",
+//   },
+//   {
+//     text: "ZfSf",
+//     id: 1723541790224,
+//     category: "TO_DO",
+//   },
+//   {
+//     text: "d",
+//     id: 1723541788280,
+//     category: "TO_DO",
+//   },
+// ];
+
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
-
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
