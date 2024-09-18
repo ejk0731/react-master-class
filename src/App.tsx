4d5cb394b4b441a856eafbe99406b7cfc34476eb
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-09-18 17:45:34 +0900

    feature: #8.7 - 8.9 : MotionValues

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,42 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { start } from "repl";
-import { useRef } from "react";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  // overflow: hidden;
-`;
-
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 1);
+  background: white;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVarients = {
   hover: { scale: 1, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
+  click: { scale: 1 },
   drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 10 } },
 };
 
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const background = useTransform(
+    x,
+    [-800, 0, 800],
+    ["linear-gradient(35deg, rgba(0, 210, 238), rgba(0, 83, 238))", "linear-gradient(35deg, rgba(238, 0, 53), rgba(221, 0, 238))", "linear-gradient(35deg, rgba(0, 238, 155), rgba(238, 178, 0))"]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxRef}
-          variants={boxVarients}
-          whileDrag="drag"
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+    <Wrapper style={{ background }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin variants={boxVarients} whileDrag="drag" whileHover="hover" whileTap="click" />
     </Wrapper>
   );
 }
