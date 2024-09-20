f9f40d302d98a908e43243d9ed2f26c8c4a98b32
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-09-20 14:10:16 +0900

    feature: #8.14 : Layout Amimation, Shared Layout animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,49 @@
 import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   background: gray;
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
 `;
 
-const box = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.5,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  width: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 9 ? 9 : prev + 1));
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => {
+    setClicked(!clicked);
   };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-  useEffect(() => {
-    console.log(back);
-  }, [back]);
-
   return (
-    <Wrapper>
-      <AnimatePresence custom={back} mode="wait">
-        {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => {
-          return i === visible ? (
-            <Box key={i} variants={box} initial="entry" animate="visible" exit="exit">
-              {i}
-            </Box>
-          ) : null;
-        })} */}
-        <Box key={visible} custom={back} variants={box} initial="entry" animate="center" exit="exit">
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevPlease}>Prev</button>
-      <button onClick={nextPlease}>Next</button>
+    <Wrapper onClick={toggleClicked}>
+      {/* Layout Amimation */}
+      <Box style={{ justifyContent: clicked ? "center" : "flex-start", alignItems: clicked ? "center" : "flex-start" }}>
+        <Circle layout />
+      </Box>
+      {/* Shared Layout animation */}
+      <Box>{!clicked ? <Circle layoutId="circle" style={{ borderRadius: 50 }} /> : null}</Box>
+      <Box>{clicked ? <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} /> : null}</Box>
     </Wrapper>
   );
 }
