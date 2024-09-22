32a05d1c1f1ed38fa22808ac4183d6efc78ac1bb
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-09-23 01:34:09 +0900

    feature: #9.6 : Home screen part 2

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,9 +1,56 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { getMovies, IGetMoviesResult } from "../api";
+import styled from "styled-components";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+  padding: 60px;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  width: 50%;
+  font-size: 20px;
+`;
 
 const Home = () => {
-  const { data, isLoading } = useQuery(["movie", "nowPlaying"], getMovies);
-  console.log(data, isLoading)
-  return <div style={{ background: "green", height: "200vh", width: "100%", color: "white" }}>홈</div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(["movie", "nowPlaying"], getMovies);
+  console.log(data, isLoading);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader></Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview> {data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 };
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -65,7 +65,7 @@ table {
 }
 body {
   font-family: "Source Sans 3", sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
+  background-color: ${(props) => props.theme.white.darker};
   color: ${(props) => props.theme.textColor};
 }
 a {
