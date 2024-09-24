c0cd12d8f4e1038c71786cfdcb07efab7c351696
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-09-24 16:59:35 +0900

    feature: #9.7 : Slider part 1

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import { getMovies, IGetMoviesResult } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background: black;
@@ -21,7 +23,8 @@ const Banner = styled.div<{ bgPhoto: string }>`
   flex-direction: column;
   color: white;
   padding: 60px;
-  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url(${(props) => props.bgPhoto});
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)),
+    url(${(props) => props.bgPhoto});
   background-size: cover;
 `;
 
@@ -35,8 +38,35 @@ const Overview = styled.p`
   font-size: 20px;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -150px;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+// vw, window.innerWidth, window.outerWidth
+const rowVariants = {
+  hidden: { x: "100vw" },
+  visible: { x: 0 },
+  exit: { x: "-100vw"},
+};
+
 const Home = () => {
   const { data, isLoading } = useQuery<IGetMoviesResult>(["movie", "nowPlaying"], getMovies);
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => setIndex((prev) => prev + 1);
   console.log(data, isLoading);
   return (
     <Wrapper>
@@ -44,10 +74,31 @@ const Home = () => {
         <Loader></Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview> {data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 4 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>
+                    {index},{i}
+                  </Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
