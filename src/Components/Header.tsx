e99834ffc214497e247a347605b8fb51a5844762
Eunji <103646256+ejk0731@users.noreply.github.com>
2024-09-24 16:33:31 +0900

    fix: change header style

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -150,8 +150,8 @@ const Header = () => {
       </Col>
       <Col>
         <Search>
-          <Input placeholder="Search movie or tv show!" animate={inputAnimation} />
-          <motion.svg onClick={toggleSearch} animate={{ x: searchOpen ? -180 : 0 }} transition={{ type: "linear" }} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <Input placeholder="Search movie or tv show!" animate={inputAnimation} initial={{ scaleX: 0 }}/>
+          <motion.svg onClick={toggleSearch} animate={{ x: searchOpen ? -210 : 0 }} transition={{ type: "linear" }} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
           </motion.svg>
         </Search>
